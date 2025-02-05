d762a1defb0c0be383bd9ee981d06ab8f6e7e424
Alvin Granvik <118818910+AlvinGranvik@users.noreply.github.com>
2025-02-05 09:05:18 +0100

    skib nib

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,34 @@ const router = express.Router()
 
 router.get("/", async (req, res) => {
   const [tweets] = await pool.promise().query(`
-    SELECT tweet.*, user.name
+    SELECT tweet.*, user.name, DATE_FORMAT(tweet.updated_at, "%Y-%m-%d %H:%i") AS date
     FROM tweet
-    JOIN user ON tweet.author_id = user.id;`)
+    JOIN user ON tweet.author_id = user.id
+    ORDER BY updated_at DESC;`)
 
   res.render("index.njk", {
-    title: "Qvixter",
+    title: "Post - All posts",
     message: "Message from routes/index.js",
     tweets: tweets,
   })
+})
+
+router.get("/new", (req, res) => {
+  res.render("new.njk", {
+    title: "How for to post guys pls help - Post the post",
+  })
+})
+
+router.post("/new", async (req, res) => {
+  const message = req.body.message
+  const author_id = 1
+  await pool
+    .promise()
+    .query("INSERT INTO tweet (message, author_id) VALUES (?, ?)", [
+      message,
+      author_id,
+    ])
+  res.redirect("/")
 })
 
 export default router
